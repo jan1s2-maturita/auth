821e2ab1a801b58c5f0e89d4df6111043769bc71
Jan Str <janstr06@gmail.com>
2024-11-10 18:47:36 +0100

    fix testing key storage - those keys ARE NOT used anywhere outside of one off testing, hence RSA-512

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,5 @@
 import jwt
-from .config import private_key
+from .config import PRIVATE_KEY_PATH
 from datetime import datetime, timedelta
 import math
 
@@ -12,7 +12,9 @@ def get_admin(id: int) -> bool:
     return True
 
 def sign_data(data):
-    return jwt.encode(data, private_key, algorithm='RS256')
+    with open(PRIVATE_KEY_PATH, "rb") as private_key_file:
+        private_key = private_key_file.read()
+        return jwt.encode(data, private_key, algorithm='RS256')
 
 def get_timestamp(days: int = 0) -> int:
     return math.floor(datetime.timestamp(datetime.now() + timedelta(days=days)))
