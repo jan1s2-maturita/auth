bb12aea81a6a5d3210bef091dce8cd3aad9dd49c
Jan Str <janstr06@gmail.com>
2024-11-18 09:00:12 +0100

    update

--- app/routers/creating.py
+++ app/routers/creating.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, HTTPException
 from ..dependencies import sign_data, get_id, get_admin, get_timestamp
 from ..models.user import User
+from bcrypt import checkpw, gensalt, hashpw
+from ..db import db
 
 router = APIRouter()
 
-@router.post("/token")
+@router.post("/login")
 def get_token(user: User):
     if not check_password(user.email, user.password):
         raise HTTPException(status_code=400, detail="Incorrect email or password")
@@ -13,6 +15,9 @@ def get_token(user: User):
 
     data = {"sub": id, "admin": admin, "exp": get_timestamp(7), "iat": get_timestamp()}
     return sign_data(data)
+
+@router.post("/register")
+
 
 # TODO
 def check_password(email: str, password: str):
