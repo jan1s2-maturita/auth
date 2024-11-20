9277e7ab042abc470291d2f14df9b4ddba858db4
Jan Str <janstr06@gmail.com>
2024-11-20 12:59:00 +0100

    add health path

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from .routers import creating
+from .routers import creating, health
 
 app = FastAPI()
 app.include_router(creating.router)
+app.include_router(health.router)
