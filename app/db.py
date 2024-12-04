3a1d4ec6d8f494bfbb9b7053204f457f08a94c80
Jan Str <janstr06@gmail.com>
2024-12-04 14:24:10 +0100

    fix

--- app/db.py
+++ app/db.py
@@ -1,4 +1,4 @@
 from .models.db_connect import Database
 from .config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
 
-db = Database(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+db = Database(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, port=DB_PORT, db_name=DB_NAME)
