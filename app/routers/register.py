8bf59520d7af4ffbfc0e7ada0b614688101811d5
Jan Str <janstr06@gmail.com>
2024-12-22 17:28:39 +0100

    update

--- app/routers/register.py
+++ app/routers/register.py
@@ -1,11 +1,15 @@
 from ..db import db
 from ..models.request import UserRegister
 from fastapi import APIRouter, HTTPException
-
+import re
 router = APIRouter()
 
 @router.post("/register")
 def register(user: UserRegister):
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", user.email):
+        raise HTTPException(status_code=400, detail="Invalid email")
+    if len(user.password) < 4:
+        raise HTTPException(status_code=400, detail="Password too short")
     db.add_user(email=user.email, password=user.password, username=user.username)
     return {"message": "User created"}
 
