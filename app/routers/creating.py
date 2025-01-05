c22df1e0252c265af9428e02ca8862736a782317
Jan Str <janstr06@gmail.com>
2025-01-05 15:18:12 +0100

    update

--- app/routers/creating.py
+++ app/routers/creating.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 from ..dependencies import sign_data, get_timestamp
 from ..models.db import User
 from bcrypt import checkpw, gensalt, hashpw
@@ -8,7 +8,7 @@ from ..models.request import UserLogin
 router = APIRouter()
 
 @router.post("/login")
-def get_token(userLogin: UserLogin):
+def get_token(userLogin: UserLogin, response: Response):
     user = db.get_user_by_creds(userLogin.username, userLogin.password)
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
@@ -16,4 +16,6 @@ def get_token(userLogin: UserLogin):
     admin = user.get_is_admin()
 
     data = {"sub": id, "admin": admin, "exp": get_timestamp(7), "iat": get_timestamp()}
+    token = sign_data(data)
+    response.set_cookie(key="token", value=token, httponly=True)
     return {"token": sign_data(data)}
