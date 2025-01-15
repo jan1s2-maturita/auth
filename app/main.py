d25c63ca05d4aa88a8d7660243b9a97e061525fa
Jan Str <janstr06@gmail.com>
2025-01-15 08:39:12 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .routers import creating, health, register
 
-app = FastAPI()
+app = FastAPI(root_path="/api/auth")
 app.include_router(creating.router)
 app.include_router(health.router)
 app.include_router(register.router)
